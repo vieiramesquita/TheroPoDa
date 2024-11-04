c277f05fb130a6f7e505dc51c5080de3348fef90
Vinícius Mesquita <vieiramesquita@gmail.com>
2024-11-04 17:46:29 -0300

    Update trend_analysis.py

--- trend_analysis.py
+++ trend_analysis.py
@@ -42,7 +42,7 @@ def extract_ts(df,dt_5days):
     #ts_a.append( df_dt['NDVI_median'].mean() )
     #ts_b.append( df_dt[df_dt['Pixel_used'] > 50]['NDVI_median'].mean() )
     #ts.append( df_dt[df_dt['Pixel_used'] >= 70]['NDVI_median'].mean() )
-    ts.append(df_dt[(df_dt['Pixel_used'] >= 70) | (df_dt['NDVI_median'] > 0)]['NDVI_median'].mean())
+    ts.append(df_dt[(df_dt['Pixel_used'] >= 70) & (df_dt['NDVI_median'] > 0)]['NDVI_median'].mean())
   
     dates.append((dt2 - relativedelta(days=8)).strftime('%Y-%m-%d'))
     #except:
